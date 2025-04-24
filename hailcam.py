162abf27910e48e820d83baae877150cf2e2bea8
Tema G. <mazy@aspentechlabs.com>
2025-04-24 17:27:03 +0200

    INIT

---
+++ hailcam.py
@@ -0,0 +1,177 @@
+import os
+import logging
+import cv2
+import time
+import board
+import adafruit_vl53l4cd
+
+from flask import Flask, render_template, Response
+from picamera2 import Picamera2
+from libcamera import controls
+from waitress import serve
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+vl53 = adafruit_vl53l4cd.VL53L4CD(i2c)
+
+# OPTIONAL: can set non-default values
+vl53.inter_measurement = 0
+vl53.timing_budget = 200
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Initialize Flask app
+app = Flask(__name__,template_folder="tpl")
+
+# Initialize Picamera2 instance
+camera = None
+camera_initialized = False
+# Still snapshot profile
+c_still = None
+c_still_size = (4608, 2592)
+# Preview stream
+c_preview = None
+c_preview_size = (1280, 1024)
+
+# read proximity sensor
+measure_distance = True
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 0.5
+font_color = (128,128, 255)  # White color
+thickness = 2
+
+
+
+# Initialize camera
+def initialize_camera():
+    global camera, camera_initialized, c_still, c_preview
+    if not camera_initialized:
+        try:
+            camera = Picamera2()
+            sensor_modes = camera.sensor_modes
+            logger.info(f"Sensor modes {sensor_modes}")
+
+            c_still = camera.create_still_configuration(
+                main={"size": c_still_size , "format": "RGB888" }  # Set desired resolution 
+            )
+
+            c_preview = camera.create_preview_configuration(
+                main={"size": c_preview_size, "format": "RGB888" }  
+            )
+            # Set manual exposure time and disable auto exposure
+#            camera.set_controls({
+#                "ExposureTime": 50000,  #  exposure
+#                "AeEnable": True,      # Disable auto exposure
+#                "AfMode": controls.AfModeEnum.Continuous
+#            })
+            camera.configure("preview")
+            camera.configure(c_preview)
+            camera_initialized = True
+            logger.info("Camera initialized successfully with custom settings.")
+        except Exception as e:
+            logger.error(f"Error initializing camera: {e}")
+            camera_initialized = False
+
+
+# Route for serving still image
+@app.route('/distance')
+def distance():
+    d = get_distance()
+    logger.info("Distance {} cm".format(d))
+    return {
+        "distance_cm": d,
+    }
+
+# Route for serving still image
+@app.route('/capture.jpg')
+def still():
+    logger.info("Snapshot requested.")
+    return Response(make_snapshot(),mimetype='image/jpeg')
+
+
+# Route for serving the video stream
+@app.route('/video_feed')
+def video_feed():
+    logger.info("Video feed requested.")
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Route for displaying the main page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#reas distance from sensor
+def get_distance():
+    vl53.clear_interrupt()
+    return vl53.distance
+
+# Generate video frames for streaming
+def generate_frames():
+    global camera, measure_distance
+    try:
+        camera.start()
+        while True:
+            # Capture a frame
+            frame = camera.capture_array()
+            position = (frame.shape[1] - 300, frame.shape[0] - 20)  # Bottom-right corner
+            if measure_distance:
+                distance = "Distance: {} cm".format(get_distance())
+                cv2.putText(frame, distance, position, font, font_scale, font_color, thickness)
+
+            # Convert the frame to JPEG
+            success, jpeg = cv2.imencode('.jpg', frame)
+            if not success:
+                logger.error("Failed to encode frame.")
+                continue
+
+            # Yield the JPEG image as part of the multipart response
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+
+            # Limit frame rate
+            time.sleep(0.2)
+
+    except Exception as e:
+        logger.error(f"Error during frame generation: {e}")
+
+# Generate video frames for streaming
+def make_snapshot():
+    global camera,c_still
+    try:
+        camera.start()
+        # Capture a frame
+        frame = camera.switch_mode_and_capture_array(c_still,"main")
+        camera.stop_recording()
+        # Convert the frame to JPEG
+        success, jpeg = cv2.imencode('.jpg', frame)
+        if not success:
+            logger.error("Failed to encode frame.")
+        else:    
+            return (jpeg.tobytes() + b'\r\n\r\n')
+
+    except Exception as e:
+        logger.error(f"Error during snaphot: {e}")
+
+
+if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = os.environ.get('PORT', '8082')
+    logger.info("Initializing Distance sensor.")
+    model_id, module_type = vl53.model_info
+    logger.info("Sensor model ID: 0x{:0X}".format(model_id))
+    logger.info("Sensor moudle Type: 0x{:0X}".format(module_type))
+    vl53.start_ranging()
+    logger.info("Initializing Camera.")
+
+    initialize_camera()  # Ensure camera is initialized
+
+    if not camera_initialized:
+        logger.error("Camera not initialized properly.")
+        exit()
+
+    logger.info(f"Starting server at {host}:{port}")
+    serve(app, host=host, port=int(port))
+
+
