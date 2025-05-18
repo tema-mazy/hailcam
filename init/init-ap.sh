39db7794934cc79bef906689b36d8feb509ea97d
Tema G. <mazy@aspentechlabs.com>
2025-05-18 20:55:05 +0200

    AP wifi priority

--- init/init-ap.sh
+++ init/init-ap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mac=`ip a | grep link/ether | awk '{ print $2;}'| awk -F ':' '{ print $4$5$6;}'`
 if [ -n "$mac" ]; then
-    nmcli device wifi hotspot ssid "HailCam AP $mac" password "12345678"
+    nmcli device wifi hotspot ssid "HailCam $mac" password "12345678"
+    nmcli conn modify Hotspot connect.autoconnect-priority 200
+    nmcli conn modify Hotspot connect.autoconnect yes
 else
     echo "Can't determine MAC"
 fi
